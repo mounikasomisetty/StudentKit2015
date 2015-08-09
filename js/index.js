5a0cef0f9a3e701339d0931f7dcbce5b3c0d609f
Mounika Somisetty <mounika.somisetty@students.iiit.ac.in>
2015-08-09 13:22:39 +0530

    Intial commit of exisiting site

---
+++ js/index.js
@@ -0,0 +1,27 @@
+// $(document).ready(function(){
+// 	heightChange();
+// 	$('.slide').hide();
+// 	$('#home').show();
+// 	$(window).resize(function(){
+// 		heightChange();
+// 	});
+	
+// });
+
+// function heightChange (argument) {
+// 	$('.max').height($(window).height());
+
+// }
+
+// function zoomOut(id) {
+// 	$('#'+id).animate({top: "60px", left: "80px", right: "80px", bottom: "60px"},"slow");
+// }
+
+// function zoomIn(id) {
+// 	$('#'+id).animate({top: "0px", left: "0px", right: "0px", bottom: "0px"},"slow");
+// }
+// function moveRight(id) {
+// 	// zoomOut('slide1');
+// 	$('.slide').hide('slide',{direction:'right'},1000);
+// 	$('#'+id).show('slide',{direction:'left'},1000);
+// }
