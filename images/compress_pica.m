05caa1cbc125adeaf944ee90ef4bb9b70fa0da91
Anurag Ghosh <ghoshanurag1995@gmail.com>
2016-07-02 16:38:05 +0530

    Polishing. Cleanup.

--- images/compress_pica.m
+++
@@ -1,9 +0,0 @@
-outdir = 'compressed';
-indir = 'medium';
-in_paths = arrayfun(@(x) x.name, dir(indir), 'UniformOutput', false);
-in_paths = in_paths(3:size(in_paths));
-for i=3:size(in_paths)
-    im = imread([indir '\' in_paths{i}]);
-    imwrite(im,[outdir '\' in_paths{i}],'Quality', 80);
-end
-
